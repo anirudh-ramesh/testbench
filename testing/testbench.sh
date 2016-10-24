14b516c76df353612ff039bd3c776b12fa6ff545
Anirudh Ramesh <anirudh.ramesh@tonboimaging.com>
2016-10-24 21:11:38 +0530

    Edit testing script

--- testing/testbench.sh
+++ testing/testbench.sh
@@ -1,14 +1,14 @@
-python main.py 
-python main.py lena.jpg _ Canny
-python main.py lena.jpg _ Canny -sigma 0
-python main.py lena.jpg _ Canny -sigma 1
-python main.py lena.jpg _ Canny -threshold 100,110
-python main.py lena.jpg _ Binarize
-python main.py lena.jpg _ Histogram
-python main.py lena.jpg _ Shear
-python main.py lena.jpg _ Rotate
-python main.py lena.jpg _ None -dump HSV
-python main.py lena.jpg _ Rotate
-python main.py lena.jpg _ Rotate -angle 90
-python main.py lena.jpg _ Shear
-python main.py lena.jpg _ Shear -angle 45
+Canny
+Canny -sigma 0
+Canny -sigma 1
+Canny -threshold 100,110
+Binarize
+Histogram
+Shear
+Rotate
+None -dump HSV
+Rotate -angle 90
+Shear
+Shear -angle 45
+Colorize -palette Jet
+Colorize -palette Summer
